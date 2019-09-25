9e13cf33e3084d6d9b11cc46ff4f3a91a18ac63e
Regelyn Cacho <rccacho@gmail.com>
2019-09-25 15:59:44 -0700

    working on walkthrough; finished sprint 4

---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import TodoModel from '../models/Todo';
+import Todos from '../components/Todos';
+import CreateTodoForm from '../components/CreateTodoForm';
+
+class TodosContainer extends Component {
+  constructor(){
+    super()
+    this.state = {
+      todos: []
+    }
+
+    this.createTodo = this.createTodo.bind(this);
+  }
+
+  createTodo(todo) {
+    let newTodo = {
+        body: todo,
+        completed: false
+    }
+    TodoModel.create(newTodo).then((res) => {
+        let todos = this.state.todos
+        let newTodos = todos.push(res.data)
+        this.setState({newTodos})
+    })
+  }
+
+  render(){
+    return (
+      <div className="todosComponent">
+        <Todos
+          todos={this.state.todos} />
+        <CreateTodoForm
+          createTodo={ this.createTodo }
+          />
+      </div>
+    )
+  }
+
+  // componentDidMount(){
+  //   this.fetchData()
+  // }
+  // fetchData(){
+  //   TodoModel.all().then( (res) => {
+  //     this.setState ({
+  //       todos: res.data.todos,
+  //       todo: ''
+  //     })
+  //   })
+  // }
+  // render(){
+  //   return (
+  //     <div className="todosComponent">
+  //       <Todos
+  //         todos={this.state.todos} />
+  //     </div>
+  //   )
+  // }
+}
+
+export default TodosContainer
---
+++ src/models/Todo.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+class TodoModel {
+  static all() {
+    let request = axios.get("https://super-crud.herokuapp.com/todos")
+    return request
+  }
+
+  static create(todo) {
+  let request = axios.post("https://super-crud.herokuapp.com/todos", todo)
+  return request
+}
+}
+
+export default TodoModel
